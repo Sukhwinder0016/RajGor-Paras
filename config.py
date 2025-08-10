c8b58e14fe3412653a296ae1b856cd850943ffc7
Sukhwinder0016 <158042570+Sukhwinder0016@users.noreply.github.com>
2025-08-10 21:19:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,7 +94,7 @@ USER_REPLY_TEXT = "ʙʀᴏ  ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ᴏᴡɴᴇʀ !!\
 OWNER_TAG = os.environ.get("OWNER_TAG", "RajGor_Paras")
 UPI_ID = os.environ.get("UPI_ID", "9328296446@ptyes")
 QR_PIC = os.environ.get("QR_PIC", "https://i.ibb.co/LXhJbg8T/photo-2025-08-10-15-21-09-7536977300156317720.jpg")
-SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/Baii_Ji")
+SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/RajGor_Paras")
 #--------------------------------------------
 #Time and its price
 #7 Days
